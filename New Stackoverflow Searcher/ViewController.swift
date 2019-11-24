30092c2a332e10434d7bd1e57c8bb691662cc32c
Berbeka Anatoly <ekzo.tech@gmail.com>
2019-11-25 02:40:24 +0500

    Initial Commit

---
+++ New Stackoverflow Searcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  New Stackoverflow Searcher
+//
+//  Created by ekzotech on 25.11.2019.
+//  Copyright © 2019 ekzotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
